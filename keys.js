ed334b039e0fb7c357c331714948795f9a0ea67f
Julian Hasse <info@julianhasse.com>
2017-10-27 15:35:31 -0400

    Update 0.1.2 [2017-10-27] fix keys.js

--- keys.js
+++ keys.js
@@ -4,7 +4,7 @@ console.log(
 	`
 ${chalk.white("Liri-Bot initialized.")} 
 ${chalk.red(".....................")}
-	`); // end template string
+	`); // 
 
 
 exports.twitterKeys = {
@@ -12,9 +12,9 @@ exports.twitterKeys = {
 	consumer_secret: 'x0s7SBwkB7sErrH9C849K2WYmgSQMBIOCrDg5qXVdEzhUYTUyw',
 	access_token_key: '922517040375779328-aCSnk9X68qzll1X6gGKLcf0nc5cWq9h',
 	access_token_secret: 'Gqx6A9F2ZYTZQBI3vTEQhNbbxoguBplCLBLvugcwwI8Eq',
-} // end twitter keys
+} // 
 
 exports.spotifyKeys = {
   id: '3fe2438535d34727a51651c7a1823635',
   secret: '2c5f1a1a11cd4d4ea41cb6f6f8b4a17b'
-} // end spotify keys
+} // 
